e0f5da8093183dcac85968eda38616f5a6f22c2c
Carlos Ortiz Rodríguez <carlosortizesp@gmail.com>
2021-02-11 12:48:37 +0100

    Cambio en el proyecto

--- tds/src/main/java/umu/tds/GUILogin.java
+++ tds/src/main/java/umu/tds/GUILogin.java
@@ -60,7 +60,7 @@ public class GUILogin {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		System.out.println("HOOOOLA CARA ALMENDRA");
+		System.out.println("HOOOOLA CARA PISTACHO");
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
